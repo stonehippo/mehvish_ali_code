f9624b07141ac5cce16e8e03504360d6fd238cf8
George White <stonehippo@gmail.com>
2025-11-25 14:34:29 -0500

    Add condition to ensure LED turns off at KeyboardInterrupt

--- photosensor.py
+++ photosensor.py
@@ -18,16 +18,19 @@ led.switch_to_output()
 delay = 0
 
 # runtime loop, goes forever
-while True:
-    # read the value from the sensor
-    value = photosensor.value
-    # adjust value to be a reasonable amount of time!
-    delay = value / 10000
-    #print value to the serial console
-    print(f"{value}, {delay}")
-    # turn on the LED for a period of time controlled by the sensor read
-    led.value = True
-    time.sleep(delay)
-    # turn off the LED for the same period of time
-    led.value = False
-    time.sleep(delay)
+try:
+    while True:
+        # read the value from the sensor
+        value = photosensor.value
+        # adjust value to be a reasonable amount of time!
+        delay = value / 10000
+        #print value to the serial console
+        print(f"{value}, {delay}")
+        # turn on the LED for a period of time controlled by the sensor read
+        led.value = True
+        time.sleep(delay)
+        # turn off the LED for the same period of time
+        led.value = False
+        time.sleep(delay)
+finally:
+    led.value = False # ensure that the LED is shut off when the code is ended via an interrupt
